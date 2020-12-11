c4c61b844cf4f19e1c588a128b82dd59f72b0042
UniverseCruiser <67194512+UniverseCruiser@users.noreply.github.com>
2020-12-11 13:34:14 +0000

    Add files via upload

--- missing_data.py
+++ missing_data.py
@@ -47,8 +47,13 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random
 ###Testing Streamlit output
 import streamlit as st
 
+st.title("Part 1:")
 st.title("Handling Missing Values")
-st.tex(" Country	Age	Salary	Purchased")
+st.title("")
+st.title("")
+st.title("")
+
+st.text(" Country	Age	Salary	Purchased")
 st.text("France	44	72000	No")
 st.text("Spain	27	48000	Yes")
 st.text("Germany	30	54000	No")
@@ -59,33 +64,57 @@ st.text("Spain		52000	No")
 st.text("France	48	79000	Yes")
 st.text("Germany	50	83000	No")
 st.text("France	37	67000	Yes")
+st.title("")
+st.title("")
 
-# st.title("Feature Scaling")
-st.text("Feature Scaling& Splitting datasets into Traing set and Test set")
+st.text("Feature Scaling & Splitting datasets into Training set and Test set")
 
-st.title("X_train")
+st.title("X train")
 st.table(X_train)
 
-st.title("Y_test")
+st.title("Y test")
 st.table(Y_test)
 
-st.title("X_test")
+st.title("X test")
 st.table(X_test)
 
-st.title("Y_train")
+st.title("Y train")
 st.table(Y_train)
 
 
 
-st.title("Handling Missing Values")
-chart_data = pd.DataFrame(
-     np.random.randn(50, 4),
-     columns=["X_train", "Y_test", "X_test", "Y_train"])
+# st.title("Chart to Show Missing Values")
+# chart_data = pd.DataFrame(
+#      np.random.randn(50, 4),
+#      columns=["X_train", "Y_test", "X_test", "Y_train"])
 
-st.bar_chart(chart_data)
-
-
+# st.bar_chart(chart_data)
 
 
+# # Append to a table
+
+# import numpy as np
+# import time
+
+
+
+# # # Get some data.
+# st.title("Appended data to visual")
+# st.text("X train")
+# data = X_train
+
+# # Show the data as a chart.
+# chart = st.line_chart(data)
+
+# # Wait 1 second, so the change is clearer.
+# time.sleep(1)
+
+# # Grab some more data.
+# # st.text("X_test")
+# data2 = X_train
+
+
+# # Append the new data to the existing chart.
+# chart.add_rows(data2)
 
 
